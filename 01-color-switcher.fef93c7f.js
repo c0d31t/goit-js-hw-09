f39047647814e4e4e67bcbb76950b94a4e693d11
c0d31t <105084462+c0d31t@users.noreply.github.com>
2023-04-07 17:34:04 +0000

    Deploying to gh-pages from @ c0d31t/goit-js-hw-09@3096505d3d21ecba10e64d667eb1cfc62d66846d 🚀

---
+++ 01-color-switcher.fef93c7f.js
@@ -0,0 +1,2 @@
+const t=document.querySelector("[data-start]"),e=document.querySelector("[data-stop]"),a=document.body;let d;t.addEventListener("click",(function(){t.disabled=!0,d=setInterval((()=>{a.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16).padStart(6,0)}`}),1e3)})),e.addEventListener("click",(function(){t.disabled=!1,clearInterval(d)}));
+//# sourceMappingURL=01-color-switcher.fef93c7f.js.map
